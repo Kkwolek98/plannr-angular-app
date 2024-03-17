249d79fbfed0dfd0aef7fe405a802f683ef72330
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-03-17 22:28:12 +0100

    fixed button width when setting loading state

--- src/lib/inputs/button/button.component.ts
+++ src/lib/inputs/button/button.component.ts
@@ -1,5 +1,13 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  inject,
+  input,
+} from "@angular/core";
 import { SpinnerComponent } from "../../spinner/spinner.component";
 
 @Component({
@@ -11,10 +19,32 @@ import { SpinnerComponent } from "../../spinner/spinner.component";
   imports: [CommonModule, SpinnerComponent],
 })
 export class ButtonComponent {
+  private readonly renderer2 = inject(Renderer2);
+
+  @ViewChild("button") button!: ElementRef<HTMLButtonElement>;
+
   public color = input<"primary" | "secondary" | "clean">("primary");
   public variant = input<"filled" | "outlined">("filled");
   public type = input<"submit" | "button">("button");
   public shape = input<"regular" | "circle">("regular");
-  public loading = input(false);
+  public loading = input<boolean, boolean>(false, {
+    transform: (value) => {
+      if (!this.button) {
+        return value;
+      }
+
+      if (value) {
+        this.renderer2.setStyle(
+          this.button.nativeElement,
+          "width",
+          `${this.button.nativeElement.getBoundingClientRect().width}px`
+        );
+      } else {
+        this.renderer2.removeStyle(this.button.nativeElement, "width");
+      }
+
+      return value;
+    },
+  });
   public disabled = input(false);
 }
