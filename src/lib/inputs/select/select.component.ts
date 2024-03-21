1fa3bb57c25272265a89b0a8792e4354293992eb
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-03-21 19:52:09 +0100

    select component

---
+++ src/lib/inputs/select/select.component.ts
@@ -0,0 +1,66 @@
+import { CommonModule } from "@angular/common";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  forwardRef,
+  inject,
+  input,
+} from "@angular/core";
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { shortId } from "../../../app/utils/short-id";
+
+@Component({
+  selector: "app-select",
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true,
+    },
+  ],
+  templateUrl: "./select.component.html",
+  styleUrl: "./select.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SelectComponent<SelectData extends []> implements ControlValueAccessor {
+  private readonly cdref = inject(ChangeDetectorRef);
+
+  public name = input<string>("");
+  public label = input<string>();
+  public labelPosition = input<"top" | "left">("top");
+  public placeholder = input<string>();
+
+  public data = input.required<SelectData>();
+  public valueKey = input.required<keyof SelectData[0]>();
+  public labelKey = input.required<keyof SelectData[0]>();
+
+  inputId: string = shortId("select");
+  onChange!: (value: string | number) => void;
+  onTouched!: () => void;
+  isDisabled: boolean = false;
+  value: string | number = "";
+
+  writeValue(value: string | number): void {
+    this.value = value;
+    this.cdref.detectChanges();
+  }
+
+  registerOnChange(fn: () => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  onModelChange() {
+    this.onChange(this.value);
+  }
+}
