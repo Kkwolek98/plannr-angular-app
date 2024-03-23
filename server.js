1ddec20f7c4599dcf465254f4b2d2d030c99d647
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-03-23 20:51:28 +0100

    server.js reversed previous changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "dist/plannr-app")));
+app.use(express.static(path.join(__dirname, "dist/plannr-app/browser")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/plannr-app/index.html"));
+  res.sendFile(path.join(__dirname, "dist/plannr-app/browser/index.html"));
 });
 
 const port = process.env["FRONTEND_PORT"] || 4200;
