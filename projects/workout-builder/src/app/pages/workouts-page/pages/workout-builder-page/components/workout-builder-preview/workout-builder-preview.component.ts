213ca8b7dd4abeb55901a55023b0b00e307e7dd3
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2024-05-01 19:49:59 +0200

    setting screen sizes for preview

--- projects/workout-builder/src/app/pages/workouts-page/pages/workout-builder-page/components/workout-builder-preview/workout-builder-preview.component.ts
+++ projects/workout-builder/src/app/pages/workouts-page/pages/workout-builder-page/components/workout-builder-preview/workout-builder-preview.component.ts
@@ -1,14 +1,26 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, WritableSignal, inject, signal } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
 import { WorkoutDisplayComponent } from "@client-components/workout-display/workout-display.component";
 import { ButtonComponent } from "@shared/inputs/button/button.component";
+import { SpinnerComponent } from "@shared/spinner/spinner.component";
+import { WorkoutBuilderService } from "@workout-builder/app/services/workout-builder.service";
 
 @Component({
   selector: "app-workout-builder-preview",
   standalone: true,
-  imports: [CommonModule, ButtonComponent, WorkoutDisplayComponent],
+  imports: [CommonModule, ButtonComponent, WorkoutDisplayComponent, SpinnerComponent],
   templateUrl: "./workout-builder-preview.component.html",
   styleUrl: "./workout-builder-preview.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WorkoutBuilderPreviewComponent {}
+export class WorkoutBuilderPreviewComponent {
+  private workoutBuilderService = inject(WorkoutBuilderService);
+
+  workoutData = toSignal(this.workoutBuilderService.data);
+  previewStyle: WritableSignal<"phone" | "tablet" | "full"> = signal("phone");
+
+  setPreviewStyle(style: "phone" | "tablet" | "full"): void {
+    this.previewStyle.set(style);
+  }
+}
